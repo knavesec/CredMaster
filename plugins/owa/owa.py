c7ed6f7efba2667c4624295ba8f6513dd83cd056
puzzlepeaches <nicholas_anastasi@yahoo.com>
2021-03-19 14:40:56 -0500

    fixed some logic

--- plugins/owa/owa.py
+++ plugins/owa/owa.py
@@ -87,7 +87,7 @@ def owa_authenticate(url, username, password, useragent, pluginargs):
 
         resp = requests.post("{}/owa/auth.owa".format(url),data=post_params,headers=headers,cookies=cookies,allow_redirects=False)
 
-        if resp.status_code == 302 and 'moved' in resp.text and 'Set-Cookie' in resp.headers:
+        if resp.status_code == 302 and 'Set-Cookie' in resp.headers and 'cadataKey' in resp.text:
             data_response['success'] = True
             data_response['output'] = '[+] SUCCESS: => {}:{}'.format(f'{domain}\{username}', password)
 
@@ -96,7 +96,7 @@ def owa_authenticate(url, username, password, useragent, pluginargs):
             data_response['output'] = '[+] SUCCESS: PASSWORD CHANGE REQUIRED: => {}:{}'.format(f'{domain}\{username}', password)
             data_response['password_change'] = True
 
-        elif 'Set-Cookie' not in resp.headers:
+        elif 'Set-Cookie' not in resp.headers or 'reason=2' in resp.text:
             data_response['success'] = False
             data_response['output'] = '[-] FAILED: {} => {}:{}'.format(resp.status_code, f'{domain}\{username}', password)
 
