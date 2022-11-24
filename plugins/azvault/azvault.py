7ea64a55ef4aede64a867119254c810778f37a4d
ZephrFish <ZephrFish@users.noreply.github.com>
2022-11-24 14:30:13 +0000

    Changes plus some fixes
    Updates:
    Added in client IDs for all Az/MS applicable modules
    
    New:
    Added MSGraph plugin/module for spraying msgraph endpoint, updated readme to reflect too.
    
    Tested and all working against Azure 24th Nov :+1:

--- plugins/azvault/azvault.py
+++ plugins/azvault/azvault.py
@@ -1,4 +1,4 @@
-import requests
+import requests, random
 import utils.utils as utils
 requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
@@ -12,9 +12,21 @@ def azvault_authenticate(url, username, password, useragent, pluginargs):
         'valid_user' : False
     }
 
+    client_ids = [
+        "4345a7b9-9a63-4910-a426-35363201d503", # alternate client_id taken from Optiv's Go365
+        "1b730954-1685-4b74-9bfd-dac224a7b894",
+        "0a7bdc5c-7b57-40be-9939-d4c5fc7cd417",
+        "1950a258-227b-4e31-a9cf-717495945fc2",
+        "00000002-0000-0000-c000-000000000000",
+        "872cd9fa-d31f-45e0-9eab-6e460a02d1f1",
+        "04b07795-8ddb-461a-bbee-02f9e1bf7b46",
+        "30cad7ca-797c-4dba-81f6-8b01f6371013"
+    ]
+    client_id = random.choice(client_ids)
+
     body = {
         'resource' : 'https://vault.azure.net',
-        'client_id' : '1b730954-1685-4b74-9bfd-dac224a7b894',
+        'client_id' : client_id,
         'client_info' : '1',
         'grant_type' : 'password',
         'username' : username,
