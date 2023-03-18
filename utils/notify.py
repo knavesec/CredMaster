874254201fc925958e7e2346cbbd5b8115056da4
0xInfection <0xinfection@gmail.com>
2023-03-18 23:04:20 +0530

    updated discord

--- utils/notify.py
+++ utils/notify.py
@@ -1,9 +1,11 @@
 import requests, json
 from datetime import datetime
+from discordwebhook import Discord
 
 def notify_success(username, password, notify_obj):
 
     slack_webhook = notify_obj['slack_webhook']
+    discord_webhook = notify_obj['discord_webhook']
     teams_webhook = notify_obj['teams_webhook']
     pushover_token = notify_obj['pushover_token']
     pushover_user = notify_obj['pushover_user']
@@ -17,6 +19,9 @@ def notify_success(username, password, notify_obj):
     if pushover_token is not None and pushover_user is not None:
         pushover_notify(username, password, operator, exclude_password, pushover_token, pushover_user)
 
+    if discord_webhook is not None:
+        discord_notify(username, password, operator, exclude_password, discord_webhook)
+
     if teams_webhook is not None:
         teams_notify(username, password, operator, exclude_password, teams_webhook)
 
@@ -27,6 +32,7 @@ def notify_success(username, password, notify_obj):
 def notify_update(message, notify_obj):
 
     slack_webhook = notify_obj['slack_webhook']
+    discord_webhook = notify_obj['discord_webhook']
     teams_webhook = notify_obj['teams_webhook']
     pushover_token = notify_obj['pushover_token']
     pushover_user = notify_obj['pushover_user']
@@ -38,6 +44,9 @@ def notify_update(message, notify_obj):
 
     if pushover_token is not None and pushover_user is not None:
         pushover_update(message, operator, pushover_token, pushover_user)
+
+    if discord_webhook is not None:
+        discord_update(message, operator, discord_webhook)
 
     if teams_webhook is not None:
         teams_update(message, operator, teams_webhook)
@@ -180,6 +189,54 @@ def slack_update(message, operator, webhook):
     else:
         print('Notification attempt to Slack failed.')
         print('Error:', response.text)
+
+
+# Function for posting username/password to Discord
+def discord_notify(username, password, operator, exclude_password, webhook):
+
+    now = datetime.now()
+    date=now.strftime("%d-%m-%Y")
+    time=now.strftime("%H:%M:%S")
+
+    op_insert = ""
+    if operator is not None:
+        op_insert = f"Operator: {operator}\n"
+
+    pwd_insert = f"Pass: {password}\n"
+    if exclude_password:
+        pwd_insert = ""
+
+    text = ("```[Valid Credentials Obtained!]\n"
+            f"{op_insert}"
+            f"User: {username}\n"
+            f"{pwd_insert}"
+            f"Date: {date}\n"
+            f"Time: {time}```")
+
+    discord = Discord(url=webhook)
+    discord.post(content=text)
+
+
+# Discord notify message
+def discord_update(message, operator, webhook):
+
+    now = datetime.now()
+    date=now.strftime("%d-%m-%Y")
+    time=now.strftime("%H:%M:%S")
+
+    op_insert = ""
+    if operator is not None:
+        op_insert = f"Operator: {operator}\n"
+
+    text = ("```[Log Entry]\n"
+            f"{op_insert}"
+            f"{message}\n"
+            f"Date: {date}\n"
+            f"Time: {time}```")
+
+    discord = Discord(url=webhook)
+    discord.post(content=text)
+
 
 # Teams notify function
 def teams_notify(username, password, operator, exclude_password, webhook):
