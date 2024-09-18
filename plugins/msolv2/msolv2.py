0d81b0d2b2f66c69ad60f3f9b7381fc1854ee466
Stephen Bradshaw <stephen.mark.bradshaw@gmail.com>
2024-09-18 12:00:48 +1000

    Fix list syntax

--- plugins/msolv2/msolv2.py
+++ plugins/msolv2/msolv2.py
@@ -44,40 +44,40 @@ def msolv2_authenticate(url, username, password, useragent, pluginargs):
 
 
     client_ids = [
-        "a40d7d7d-59aa-447e-a655-679a4107e548" # Accounts Control UI
-        "be1918be-3fe3-4be9-b32b-b542fc27f02e" # M365 Compliance Drive Client
-        "4813382a-8fa7-425e-ab75-3b753aab3abb" # Microsoft Authenticator App
-        "04b07795-8ddb-461a-bbee-02f9e1bf7b46" # Microsoft Azure CLI
-        "1950a258-227b-4e31-a9cf-717495945fc2" # Microsoft Azure PowerShell
-        "cf36b471-5b44-428c-9ce7-313bf84528de" # Microsoft Bing Search
-        "2d7f3606-b07d-41d1-b9d2-0d0c9296a6e8" # Microsoft Bing Search for Microsoft Edge
-        "cab96880-db5b-4e15-90a7-f3f1d62ffe39" # Microsoft Defender Platform
-        "dd47d17a-3194-4d86-bfd5-c6ae6f5651e3" # Microsoft Defender for Mobile
-        "f44b1140-bc5e-48c6-8dc0-5cf5a53c0e34" # Microsoft Edge
-        "d7b530a4-7680-4c23-a8bf-c52c121d2e87" # Microsoft Edge Enterprise New Tab Page
-        "57fcbcfa-7cee-4eb1-8b25-12d2030b4ee0" # Microsoft Flow
-        "9ba1a5c7-f17a-4de9-a1f1-6178c8d51223" # Microsoft Intune Company Portal
-        "d3590ed6-52b3-4102-aeff-aad2292ab01c" # Microsoft Office
-        "66375f6b-983f-4c2c-9701-d680650f588f" # Microsoft Planner
-        "c0d2a505-13b8-4ae0-aa9e-cddd5eab0b12" # Microsoft Power BI
-        "844cca35-0656-46ce-b636-13f48b0eecbd" # Microsoft Stream Mobile Native
-        "1fec8e78-bce4-4aaf-ab1b-5451cc387264" # Microsoft Teams
-        "87749df4-7ccf-48f8-aa87-704bad0e0e16" # Microsoft Teams - Device Admin Agent
-        "22098786-6e16-43cc-a27d-191a01a1e3b5" # Microsoft To-Do client
-        "eb539595-3fe1-474e-9c1d-feb3625d1be5" # Microsoft Tunnel
-        "57336123-6e14-4acc-8dcf-287b6088aa28" # Microsoft Whiteboard Client
-        "00b41c95-dab0-4487-9791-b9d2c32c80f2" # Office 365 Management
-        "0ec893e0-5785-4de6-99da-4ed124e5296c" # Office UWP PWA
-        "b26aadf8-566f-4478-926f-589f601d9c74" # OneDrive
-        "ab9b8c07-8f02-4f72-87fa-80105867a763" # OneDrive SyncEngine
-        "af124e86-4e96-495a-b70a-90f90ab96707" # OneDrive iOS App
-        "e9b154d0-7658-433b-bb25-6b8e0a8a7c59" # Outlook Lite
-        "27922004-5251-4030-b22d-91ecd9a37ea4" # Outlook Mobile
-        "4e291c71-d680-4d0e-9640-0a3358e31177" # PowerApps
-        "d326c1ce-6cc6-4de2-bebc-4591e5e13ef0" # SharePoint
-        "f05ff7c9-f75a-4acd-a3b5-f4b6a870245d" # SharePoint Android
-        "872cd9fa-d31f-45e0-9eab-6e460a02d1f1" # Visual Studio
-        "26a7ee05-5602-4d76-a7ba-eae8b7b67941" # Windows Search
+        "a40d7d7d-59aa-447e-a655-679a4107e548", # Accounts Control UI
+        "be1918be-3fe3-4be9-b32b-b542fc27f02e", # M365 Compliance Drive Client
+        "4813382a-8fa7-425e-ab75-3b753aab3abb", # Microsoft Authenticator App
+        "04b07795-8ddb-461a-bbee-02f9e1bf7b46", # Microsoft Azure CLI
+        "1950a258-227b-4e31-a9cf-717495945fc2", # Microsoft Azure PowerShell
+        "cf36b471-5b44-428c-9ce7-313bf84528de", # Microsoft Bing Search
+        "2d7f3606-b07d-41d1-b9d2-0d0c9296a6e8", # Microsoft Bing Search for Microsoft Edge
+        "cab96880-db5b-4e15-90a7-f3f1d62ffe39", # Microsoft Defender Platform
+        "dd47d17a-3194-4d86-bfd5-c6ae6f5651e3", # Microsoft Defender for Mobile
+        "f44b1140-bc5e-48c6-8dc0-5cf5a53c0e34", # Microsoft Edge
+        "d7b530a4-7680-4c23-a8bf-c52c121d2e87", # Microsoft Edge Enterprise New Tab Page
+        "57fcbcfa-7cee-4eb1-8b25-12d2030b4ee0", # Microsoft Flow
+        "9ba1a5c7-f17a-4de9-a1f1-6178c8d51223", # Microsoft Intune Company Portal
+        "d3590ed6-52b3-4102-aeff-aad2292ab01c", # Microsoft Office
+        "66375f6b-983f-4c2c-9701-d680650f588f", # Microsoft Planner
+        "c0d2a505-13b8-4ae0-aa9e-cddd5eab0b12", # Microsoft Power BI
+        "844cca35-0656-46ce-b636-13f48b0eecbd", # Microsoft Stream Mobile Native
+        "1fec8e78-bce4-4aaf-ab1b-5451cc387264", # Microsoft Teams
+        "87749df4-7ccf-48f8-aa87-704bad0e0e16", # Microsoft Teams - Device Admin Agent
+        "22098786-6e16-43cc-a27d-191a01a1e3b5", # Microsoft To-Do client
+        "eb539595-3fe1-474e-9c1d-feb3625d1be5", # Microsoft Tunnel
+        "57336123-6e14-4acc-8dcf-287b6088aa28", # Microsoft Whiteboard Client
+        "00b41c95-dab0-4487-9791-b9d2c32c80f2", # Office 365 Management
+        "0ec893e0-5785-4de6-99da-4ed124e5296c", # Office UWP PWA
+        "b26aadf8-566f-4478-926f-589f601d9c74", # OneDrive
+        "ab9b8c07-8f02-4f72-87fa-80105867a763", # OneDrive SyncEngine
+        "af124e86-4e96-495a-b70a-90f90ab96707", # OneDrive iOS App
+        "e9b154d0-7658-433b-bb25-6b8e0a8a7c59", # Outlook Lite
+        "27922004-5251-4030-b22d-91ecd9a37ea4", # Outlook Mobile
+        "4e291c71-d680-4d0e-9640-0a3358e31177", # PowerApps
+        "d326c1ce-6cc6-4de2-bebc-4591e5e13ef0", # SharePoint
+        "f05ff7c9-f75a-4acd-a3b5-f4b6a870245d", # SharePoint Android
+        "872cd9fa-d31f-45e0-9eab-6e460a02d1f1", # Visual Studio
+        "26a7ee05-5602-4d76-a7ba-eae8b7b67941", # Windows Search
         "a569458c-7f2b-45cb-bab9-b7dee514d112" # Yammer iPhone
     ]
     
